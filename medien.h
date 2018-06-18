0010660bc0ce5190092ddeb83d4eb1bca3c66b09
p-will <paul.willam@gmx.de>
2018-06-18 14:52:32 +0200

    Medien Klasse Deklaration

--- medien.h
+++ medien.h
@@ -1,10 +1,37 @@
 //Paul Willam 44093 Bachelor 062
 
-
-
-
+/*
+Headerfile für die Übergeordnete Medienklasse
+*/
 
 #ifndef MEDIEN_H
 #define MEDIEN_H
+
+#include <string>
+
+class Medien 
+{
+    private:
+    std::string Kuenstler; //enthält den Namen des Autors/Interpreten
+    std::string Titel; //enthält Namen des Buchs/Songs/Albums
+    std::string Jahr; //enthält Erscheinungs-/Veröffentlichungsjahr
+    std::string ISBN; //enthält einmalige ISBN des Mediums
+    std::string Rueckgabe; //enthält fälliges Rückgabedatum
+    public:
+    Medien(); //Konstruktor
+    ~Medien(); //Destruktor
+    void show() const; //zeigt alle Informationen des Mediums an
+    std::string getKuenstler() const; //Künstler getter
+    std::string getTitel() const; //Titel getter
+    std::string getJahr() const; //Veröffentlichungsjahr getter
+    std::string getISBN() const; //ISBN getter
+    std::string getRueckgabe() const; //Rückgabedatum getter
+    int setKuenstler(); //Künstler setter
+    int setTitel(); //Titel setter
+    int setJahr(); //Veröffentlichungsjahr setter
+    int setISBN(); //ISBN setter
+    int setRueckgabe(); //Rückgabedatum setter
+    int loeschen(); //löscht Medium inklusive in Ausleihe befindlicher Medien
+};
 
 #endif
